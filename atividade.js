95bace5933691a9a9754c6f73d0f816459ca560f
Jackeline Silva <jackelinesilva.tech@gmail.com>
2024-09-18 18:00:19 +0000

    criamos algoritmo boletim

---
+++ atividade.js
@@ -0,0 +1,99 @@
+//Código 1 do primeiro exercício de Javascript//
+
+/*let nome = "jackeline";
+let idade = 26;
+
+console.log ("Olá, meu nome é" + "  " + nome + "  " + "e tenho" + " " + idade + "  " + "anos"); */
+
+//Código 2 do primeiro exercício de Javascript//
+
+/*let nome = "jackeline";
+let cidade = "recife";
+
+console.log (nome + " é de" + "  "  + cidade); */
+
+//Código 3 do primeiro exercício de Javascript//
+
+/*let num1 = 2;
+let num2 = 10;
+let soma = num1 + num2;
+
+console.log ("2 + 10 =" +  "  " + soma); */
+
+/*let num1 = 2;
+let num2 = 10;
+let subtracao = num1 - num2;
+
+console.log ("2 - 10 =" +  "  " + subtracao); */
+
+/*let num1 = 2;
+let num2 = 10;
+let multificacao = num1 * num2;
+
+console.log ("2 * 10 =" +  "  " + multificacao); */
+
+/*let num1 = 2;
+let num2 = 10;
+let divisao = num1 / num2;
+
+console.log ("2 / 10 =" +  "  " + divisao); */
+
+//Código 4 do primeiro exercício de Javascript-coloca medida//
+
+/*let base = 21;
+let altura = 11;
+let area = base * altura / 2;
+
+console.log ("A área do triangulo é" + "  " + area); */
+
+//Código 5 do primeiro exercício de Javascript//
+
+/*const nota1 = 7;
+const nota2 = 8;
+const nota3 = 10;
+const media = (nota1 + nota2 + nota3)/3
+console.log (`O resultado da média é... ${media.toFixed(2)} `) */
+
+//Código 6 (desconto) do primeiro exercício de Javascript//
+
+/*let produto = "camisa";
+let precoOriginal = 100;
+let desconto = 15;
+let precoFinal = precoOriginal - desconto;
+
+console.log ("Com o desconto de 15%, o preço final da camisa fica = " + precoFinal + "  " + "reais"); */
+
+//Código 7 (imposto de renda) do primeiro exercício de Javascript//
+
+/*let salarioBruto = 5000;
+let parcelaImpostoDeRenda = 55;
+
+if (salarioBruto <= 2112) { 
+    console.log ("Você está insento");
+} 
+else if (salarioBruto <= 2826.65) {
+    parcelaImpostoDeRenda = salarioBruto * 0.075 - 158.40
+    console.log ("Você deverá pagar R$"+ parcelaImpostoDeRenda);
+}
+else if (salarioBruto <= 3751.05) { 
+    parcelaImpostoDeRenda = salarioBruto * 0.225 - 158.40
+    console.log ("Você deverá pagar R$"+ parcelaImpostoDeRenda);
+}
+else if (salarioBruto <= 4664.68) {
+    parcelaImpostoDeRenda = salarioBruto * 0.225 - 651.73
+    console.log ("Você deverá pagar R$"+ parcelaImpostoDeRenda);
+}
+else {
+    parcelaImpostoDeRenda = salarioBruto * 0.275 - 884.96
+    console.log ("Você deverá pagar R$"+ parcelaImpostoDeRenda);   
+} */
+
+//Código 8 (conversor de moedas) do primeiro exercício de Javascript//
+
+/*let valorMoeda = 1;
+valorMoeda = 0.1824	;
+realParaDolar = valorMoeda * 2000
+console.log (" Este é o valor da multiplicação:" + "  " + realParaDolar) */
+
+
+
---
+++ aula.js
@@ -0,0 +1,65 @@
+
+//Este código foi para mostrar que não posso redeclarar o let//
+
+/*let rotulo= "Natalya";
+let rotulo = 12345;
+console.log(rotulo);
+*/
+
+//Este código foi para mostrar concatenação de strings//
+
+/*console.log('Hello' + ' '+  'World')*/
+
+//Este código foi para mostrar concatenação de strings//
+
+/*myName = "Maria";
+console.log("Hello" + ' ' + myName);
+*/
+
+//Este código foi para mostrar que posso incluir palavras chaves prontas//
+
+/*myName2 = "2"
+console.log(´Hello ${myName2}´);
+*/
+
+//Este código foi para mostrar uma soma//
+
+/*const numero1 = 10;
+const numero2 = 20;
+const soma = numero1 + numero2
+console.log(soma); */
+
+//Este código foi para mostrar uma subtração//
+
+/*const numero1 = 21
+const numero2 = 10
+const subtracao = numero1 - numero2
+console.log(subtracao); */
+
+//Este código foi para mostrar uma multiplicação//
+
+/*const numero1 = 21
+const numero2 = 10
+const multiplicacao = numero1 * numero2
+console.log(multiplicacao);  */
+
+//Este código foi para mostrar uma divisão usando o primeiro tipo de concatenação//
+/*const numero1 = 21
+const numero2 = 10
+const divisao = numero1 / numero2
+console.log("O resultado de sua divisão foi=" + '  ' + divisao); */
+
+//Este código foi para mostrar uma divisão usando o segundo tipo de concatenação//
+
+/*const numero1 = 21
+const numero2 = 10
+const divisao = numero1 / numero2
+console.log(`O resultado de sua divisão foi ${divisao}`);  */
+
+//Este código foi para mostrar uma divisão//
+/*const numero1 = 21
+const numero2 = 10
+const divisao = numero1 / numero2
+const modulo= divisao
+console.log(modulo); */
+
